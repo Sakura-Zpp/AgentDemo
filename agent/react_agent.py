cb2f3614ee419c5b288d021d286f4aa8e5f24724
sakura <756891309@qq.com>
2026-03-22 17:38:25 +0800

    更新

--- agent/react_agent.py
+++ agent/react_agent.py
@@ -13,28 +13,47 @@ class ReactAgent:
         self.agent = None
         self.tools = None
         self._initialized = False
+        self.init_timeout = 30
+        self.execute_timeout = 120
 
-    async def initialize(self):
+    async def initialize(self,timeout: int = None):
         if self._initialized:
             logger.info("智能体已初始化，跳过")
             return
+
+        limit = timeout if timeout is not None else self.init_timeout
+
+
         try:
-            self.tools = await get_all_tools()
-            logger.info(f"工具加载，共有 {len(self.tools)} 个")
+            self.tools = await asyncio.wait_for(get_all_tools(), timeout=limit)
+            logger.info(f"✅ MCP 工具加载成功，共有 {len(self.tools)} 个")
+
+        except asyncio.TimeoutError:
+            #防止调用MCP服务超时，进行工具降级
+            logger.error(f"MCP工具加载超时 {limit} s,工具降级,只使用基础工具")
+            self.tools = [rag_search, fill_context_report, get_weather]
+
         except Exception as e:
             #防止调用MCP服务失败，进行工具降级
-            logger.error(f"MCP工具加载失败: {e} ,工具降级，只使用基础工具")
+            logger.error(f"MCP工具加载失败: {e} ,工具降级,只使用基础工具")
             self.tools = [rag_search, fill_context_report, get_weather]
             logger.info(f"基础工具加载成功，共有 {len(self.tools)} 个")
 
-        self.agent = create_agent(
-            model=chat_model,
-            system_prompt=load_system_prompts(),
-            tools=self.tools,
-            middleware=[monitor_tool, log_before_model, report_prompt_switch],
-        )
+        try:
+            self.agent = create_agent(
+                model=chat_model,
+                system_prompt=load_system_prompts(),
+                tools=self.tools,
+                middleware=[monitor_tool, log_before_model, report_prompt_switch],
+            )
 
-        self._initialized = True
+            self._initialized = True
+
+        except Exception as e:
+            logger.error(f"Agent创建失败{e}")
+            raise RuntimeError("Agent初始化失败")
+
+
 
 #处理文本返回类型
     def extract_content(self,content):
@@ -62,7 +81,9 @@ class ReactAgent:
         #其他类型转回为字符串
         return str(content)
 
-    async def execute_stream(self,query: str):
+    async def execute_stream(self,query: str,timeout: int = None):
+
+        limit = timeout if timeout is not None else self.execute_timeout
 
         if not self._initialized or self.agent is None:
             raise RuntimeError("请先调用 await agent.initialize()")
@@ -73,30 +94,27 @@ class ReactAgent:
             ]
         }
 
-        async for chunk in self.agent.astream(input_dict,stream_mode="values",context={"report":False}):
-            #确保消息不为空
-            if not chunk.get("messages"):
-                continue
+        try:
+            async with asyncio.timeout(limit):
+                async for chunk in self.agent.astream(input_dict,stream_mode="values",context={"report":False}):
+                    #确保消息不为空
+                    if not chunk.get("messages"):
+                        continue
 
-            last_messages = chunk["messages"][-1]
+                    last_messages = chunk["messages"][-1]
 
-            #获取last_messages的类型
-            content = last_messages.content if hasattr(last_messages, 'content') else last_messages
-            logger.info(f"获取到的类型为{type(content)}")
-            # 使用辅助方法提取文本
-            text = self.extract_content(content)
+                    #获取last_messages的类型
+                    content = last_messages.content if hasattr(last_messages, 'content') else last_messages
+                    logger.info(f"获取到的类型为{type(content)}")
+                    # 使用辅助方法提取文本
+                    text = self.extract_content(content)
 
-            if text:
-               yield text.strip() + "\n"
+                    if text:
+                       yield text.strip() + "\n"
 
-if __name__ == '__main__':
-
-    async def main():
-        print("程序启动\n")
-        agent = ReactAgent()
-        await agent.initialize()
-        print(f"工具数量 = {len(agent.tools) if agent.tools else 0}")
-        query = "公司理念与行为总则第五条是什么，并且生成一份关于智源科技的全年财报报告，并生成一份图表"
-        async for chunk in agent.execute_stream(query):
-            print(chunk)
-    asyncio.run(main())
+        except asyncio.TimeoutError:
+            logger.error(f"执行请求超时 {limit} s")
+            yield "\n 请求超时，请稍后重试。"
+        except Exception as e:
+            logger.error(f"{e}")
+            yield "\n 系统错误"
